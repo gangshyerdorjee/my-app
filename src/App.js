97d6ca8bb27999907c1e1c29016204b110de34d8
Gangshyer <you@example.com>
2023-03-13 15:49:55 +0545

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import Forgotpassword from './pages/Forgotpassword';
 import Signup from './pages/Signup';
 import { Resetpassword } from './pages/Resetpassword';
 import SingleBlog from "./pages/SingleBlog";
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import RefundPolicy from './pages/RefundPolicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import TermsAndConditions from './pages/TermsAndConditions';
+import SingleProduct from './pages/SingleProduct';
 
 function App() {
   return (
@@ -25,6 +30,7 @@ function App() {
                 <Route path='about' element={<About />} />
                 <Route path='contact' element={<Contact/>}/>
                 <Route path='store' element={<OurStore/>}/>
+                <Route path='store/product/:id' element={<SingleProduct/>}/>
                 <Route path='blog' element={<Blog/>}/>
                 <Route path='blog/:id' element={<SingleBlog/>}/>
                 <Route path='compare-product' element={<CompareProduct/>}/>
@@ -33,6 +39,10 @@ function App() {
                 <Route path='forgot-password' element={<Forgotpassword/>}/>
                 <Route path='signup' element={<Signup/>}/>
                 <Route path='reset-password' element={<Resetpassword/>}/>
+                <Route path='privacy-policy' element={<PrivacyPolicy/>}/>
+                <Route path='refund-policy' element={<RefundPolicy/>}/>
+                <Route path='shipping-policy' element={<ShippingPolicy/>}/>
+                <Route path='term-conditions' element={<TermsAndConditions/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta'
+import { Link } from 'react-router-dom'
+import { HiOutlineArrowLeft } from "react-icons/hi"
 
 const Singleblog = () => {
   return (
@@ -10,7 +12,16 @@ const Singleblog = () => {
             <div className='blog-wrapper home-wrapper-2 py-5'>
                 <div className='container-xxl'>
                     <div className='row'>
-                        <div className='col-12'></div>
+                        <div className='col-12'>
+                          <div className='single-blog-card'>
+                            <Link to='/blog' className='d-flex align-items-center gap-10'><HiOutlineArrowLeft className='fs-4'/>Go back to Blogs</Link>
+                            <h3 className='title'>
+                            Coronavirus latest: at a glance
+                            </h3>
+                            <img src='/images/blog-1.jpg' className='img-fluid w-100 my-4' alt='blog'/>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
